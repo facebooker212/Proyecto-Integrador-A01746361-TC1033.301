90d3847cb1a4310d04a30dafade52d551b24d853
Victor Martinez <42659775+facebooker212@users.noreply.github.com>
2020-12-01 20:25:41 -0600

    Add files via upload
    Parte del desarrollo del proyecto parte individual

--- clases.h
+++ clases.h
@@ -1,86 +1,76 @@
+//Proyecto integrador
+//Victor Martínez Román - A01746361
+//Lorena A Solís de los Santos - A01746602
 using namespace std;
 
-class Habitacion {
+//Parte del codigo hecha por: Victor Martinez A01746361
+class Credencial {
   private:
-      int numeroHabitacion;
-      string tamHabitacion;
+      int vigenciaDia;
+      int vigenciaMes;
+      string areaC;
   public:
-      Habitacion() = default;
-      Habitacion(int numeroHabitacion, string tamHabitacion) {
-        this->numeroHabitacion = numeroHabitacion;
-        this->tamHabitacion = tamHabitacion;
-      }
-      int getnumeroHabitacion() {return numeroHabitacion;}
-      string gettamHabitacion() {return tamHabitacion;}
-      void setnumeroHabitacion(int numeroHabitacion) {}
-      void settamHabitacion(string tamHabitacion) {}
-      void vernumeroHab() {
-        cout << "Numero de habitacion: " << numeroHabitacion << endl;
-      }
-      void vertamHabitacion() {
-        cout << "Tam habitacion: " << tamHabitacion << endl;
-      }
+    Credencial() = default;
+    Credencial(int vigenciaDia, int vigenciaMes, string areaC) {
+      this->vigenciaDia = vigenciaDia;
+      this->vigenciaMes = vigenciaMes;
+      this->areaC = areaC;
+    }
+    //Metodos get
+    int getVigenciaDia() {return vigenciaDia;}
+    int getVigenciaMes() {return vigenciaMes;}
+    string getAreaC() {return areaC;}
+    //Metodos set
+    void setVigencia(int vigenciaDia) {}
+    void setEnmicada(int vigenciaMes) {}
+    void setArea(string areaC) {}
+    //Metodos adicionales
+    void verVigenciaDia() {
+      cout << "VigenciaDia: " << vigenciaDia << endl;
+    }
+    void verVigenciaMes() {
+      cout << "VigenciaMes: " << vigenciaMes << endl;
+    }
+    void verAreaC() {
+      cout << "Area: " << areaC << endl;
+    }
 };
+//Termina parte del codigo hecha por: Victor Martinez A01746361
 
-class Huesped {
-  private:
-      string nombreHuesped;
-      string telefonoHuesped;
-      float cuentaHuesped;
-      Habitacion habitacion;
-  public:
-      Huesped() = default;
-      Huesped(string nombreHuesped, string telefonoHuesped, float cuentaHuesped, Habitacion& habitacionH) {
-        this->nombreHuesped = nombreHuesped;
-        this->telefonoHuesped = telefonoHuesped;
-        this->cuentaHuesped = cuentaHuesped;
-        this->habitacion = habitacionH;
-      }
-      string getnombreHuesped() {return nombreHuesped;}
-      string gettelefonoHuesped() {return telefonoHuesped;}
-      float getcuentaHuesped() {return cuentaHuesped;}
-      void setnombreHuesped(string nombreHuesped) {}
-      void settelefonoHuesped(string telefonoHuesped) {}
-      void setcuentaHuesped(float cuentaHuesped) {}
-      void vernombreH() {
-        cout << "Nombre huesped: " << nombreHuesped << endl;
-      }
-      void vertelefonoH() {
-        cout << "Telefono huesped: " << telefonoHuesped << endl;
-      }
-      void vercuentaH() {
-        cout << "Cuenta huesped: " << cuentaHuesped << endl;
-      }
-      void verHabitacion() {
-        cout << "Habitacion: " << habitacion.getnumeroHabitacion() << endl;
-      }
-};
-
+//Las siguientes clases conforman la parte hecha en equipo
 class Empleado {
 private:
     string nombreEmpleado;
     string matriculaEmpleado;
     float salarioEmpleado;
+    Credencial credencial;
 public:
     Empleado() = default;
-    Empleado(string nombreEmpleado, string matriculaEmpleado, float salarioEmpleado) {
+    Empleado(string nombreEmpleado, string matriculaEmpleado, float salarioEmpleado, Credencial credencial) {
       this->nombreEmpleado = nombreEmpleado;
       this->matriculaEmpleado = matriculaEmpleado;
       this->salarioEmpleado = salarioEmpleado;
+      this->credencial = credencial;
     }
-    string getnombreEmpleado() {return nombreEmpleado;}
-    string getmatriculaEmpleado() {return matriculaEmpleado;}
-    float getsalarioEmpleado() {return salarioEmpleado;}
-    void setnombreEmpleado(string nombreEmpleado) {}
-    void setmatriculaEmpleado(string matriculaEmpleado) {}
-    void setsalarioEmpleado(float salarioEmpleado) {}
-    void vernombreE() {
+    //Metodos get
+    string getNombreEmpleado() {return nombreEmpleado;}
+    string getMatriculaEmpleado() {return matriculaEmpleado;}
+    float getSalarioEmpleado() {return salarioEmpleado;}
+    //Metodos set
+    void setNombreEmpleado(string nombreEmpleado) {}
+    void setMatriculaEmpleado(string matriculaEmpleado) {}
+    void setSalarioEmpleado(float salarioEmpleado) {}
+    //Metodos adicionales
+    void verCredencial() {
+      cout << "VigenciaDia: " << credencial.getVigenciaDia() << " VigenciaMes: " << credencial.getVigenciaMes() << " Area: " << credencial.getAreaC() << endl;
+    }
+    void verNombreE() {
       cout << "Nombre empleado: " << nombreEmpleado << endl;
     }
-    void vermatriculaE() {
+    void verMatriculaE() {
       cout << "Matricula empleado: " << matriculaEmpleado << endl;
     }
-    void versalarioE() {
+    void verSalarioE() {
       cout << "Salario empleado: " << salarioEmpleado << endl;
     }
 };
@@ -97,19 +87,111 @@ class Platillo {
       this->ingrediente = ingrediente;
       this->precioPlatillo = precioPlatillo;
     }
-    string getnombrePlatillo() {return nombrePlatillo;}
-    string getingrediente() {return ingrediente;}
-    float getprecioPlatillo() {return precioPlatillo;}
-    void setnombrePlatillo(string nombrePlatillo) {}
-    void setingrediente(string ingrediente) {}
-    void setprecioPlatillo(float precioPlatillo) {}
-    void vernombreP() {
+    //Metodos get
+    string getNombrePlatillo() {return nombrePlatillo;}
+    string getIngredientes() {return ingrediente;}
+    float getPrecioPlatillo() {return precioPlatillo;}
+    //Metodos set
+    void setNombrePlatillo(string nombrePlatillo) {}
+    void setIngredientes(string ingrediente) {}
+    void setPrecioPlatillo(float precioPlatillo) {}
+    //Metodos adicionales
+    void verNombreP() {
       cout << "Nombre platillo: " << nombrePlatillo << endl;
     }
-    void verIngrediente() {
+    void verIngredientes() {
       cout << "Ingredientes: " << ingrediente << endl;
     }
-    void verprecioP() {
-      cout << "Precio: " << precioPlatillo << endl;
+    void verPrecioP() {
+      cout << "Precio del platillo: " << precioPlatillo << endl;
     }
+};
+
+class Habitacion {
+  private:
+      int numeroHabitacion;
+      string tamHabitacion;
+      float hospedaje;
+  public:
+      Habitacion() = default;
+      Habitacion(int numeroHabitacion, string tamHabitacion, float hospedaje) {
+        this->numeroHabitacion = numeroHabitacion;
+        this->tamHabitacion = tamHabitacion;
+        this->hospedaje = hospedaje;
+      }
+      //Metodos get
+      int getNumeroHabitacion() {return numeroHabitacion;}
+      string getTamHabitacion() {return tamHabitacion;}
+      float getHospedaje() {return hospedaje;}
+      //Metodos set
+      void setNumeroHabitacion(int numeroHabitacion) {}
+      void setTamHabitacion(string tamHabitacion) {}
+      void setHospedaje(float hospedaje) {}
+      //Metodos adicionales
+      void verNumeroHab() {
+        cout << "Número de habitación: " << numeroHabitacion << endl;
+      }
+      void verTamHabitacion() {
+        cout << "Tamaño habitación: " << tamHabitacion << endl;
+      }
+      float verHospedaje(string tamHabitacion, float hospedaje) {
+        float costo;
+        if (tamHabitacion == "Chica") {
+          hospedaje = hospedaje * 100;
+          costo = 100;
+        }
+        else if (tamHabitacion == "Mediana") {
+          hospedaje = hospedaje * 300;
+          costo = 300;
+        }
+        else if (tamHabitacion == "Grande") {
+          hospedaje = hospedaje * 500;
+          costo = 500;
+        }
+        else {
+          hospedaje = 0;
+          cout << "Tamaño no valido" << endl;
+        }
+        cout << "Costo por noche: " << costo << endl;
+        cout << "Costo del hospedaje: " << hospedaje << endl;
+        return hospedaje;
+      }
+};
+
+class Huesped {
+  private:
+      string nombreHuesped;
+      string telefonoHuesped;
+      float cuentaHuesped;
+      Habitacion habitacion;
+      Platillo platillo;
+  public:
+      Huesped() = default;
+      Huesped(string nombreHuesped, string telefonoHuesped, float cuentaHuesped, Habitacion& habitacionH, Platillo& platillo1) {
+        this->nombreHuesped = nombreHuesped;
+        this->telefonoHuesped = telefonoHuesped;
+        this->cuentaHuesped = cuentaHuesped;
+        this->habitacion = habitacionH;
+        this->platillo = platillo1;
+      }
+      //Metodos get
+      string getNombreHuesped() {return nombreHuesped;}
+      string getTelefonoHuesped() {return telefonoHuesped;}
+      float getCuentaHuesped() {return cuentaHuesped;}
+      //Metodos set
+      void setNombreHuesped(string nombreHuesped) {}
+      void setTelefonoHuesped(string telefonoHuesped) {}
+      void setCuentaHuesped(float cuentaHuesped) {}
+      //Metodos adicionales
+      void verNombreH() {
+        cout << "Nombre huésped: " << nombreHuesped << endl;
+      }
+      void verTelefonoH() {
+        cout << "Teléfono huésped: " << telefonoHuesped << endl;
+      }
+      float verCuentaH() {
+        float Total;
+        Total = habitacion.verHospedaje(habitacion.getTamHabitacion(), habitacion.getHospedaje()) + platillo.getPrecioPlatillo();
+        cout << "Total a pagar: " << Total << endl;
+      }
 };
--- proyecto.cpp
+++ proyecto.cpp
@@ -1,24 +1,179 @@
+//Proyecto integrador
+//Victor Martínez Román - A01746361
+//Lorena A Solís de los Santos - A01746602
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
 #include "clases.h"
 using namespace std;
 
+//Funcion para generar un numero de habitacion
+int numHab() {
+  char disc;
+  int num;
+  cout << "¿Requiere usar el lugar para discapacitados?: (Y/N) "; cin >> disc;
+  switch (disc) {
+    case 'Y':
+      num = 1;
+      return num;
+    case 'N':
+      srand(time(NULL));
+      num = 1 + rand() % 20;
+      return num;
+    default:
+      cout << "Opcion inválida" << endl;
+      numHab();
+  }
+}
+
+//Funcion para desplegar y elegir un platillo
+string menuP() {
+  string platillo;
+  int opcion;
+  cout << "Seleccione un platillo de la lista: \n";
+  cout << "<1> Hamburguesa \n";
+  cout << "<2> Ensalada \n";
+  cout << "<3> Chilaquiles \n";
+  cout << "Opcion: "; cin >> opcion;
+  switch (opcion) {
+    case 1:
+      platillo = "Hamburguesa";
+      return platillo;
+    case 2:
+      platillo = "Ensalada";
+      return platillo;
+    case 3:
+      platillo = "Chilaquiles";
+      return platillo;
+    default:
+      cout << "Opcion inválida" << endl;
+      menuP();
+  }
+}
+
+//Funcion para definir los ingredientes segun el platillo
+string menuIngredientes(string p) {
+  string ingredientes;
+  if (p == "Hamburguesa") {
+    ingredientes = "Pan, Carne, Queso, Jitomate, Lechuga";
+    return ingredientes;
+  }
+  else if (p == "Ensalada") {
+    ingredientes = "Lechuga, Zanahorita, Jitomate, Queso, Manzana";
+    return ingredientes;
+  }
+  else if (p == "Chilaquiles") {
+    ingredientes = "Salsa verde, Totopos, Queso, Crema, Pollo, Cebolla";
+    return ingredientes;
+  }
+}
+
+//Funcion para definir el precio segun el platillo
+float menuPrecioPlatillos(string p) {
+  float precio;
+  if (p == "Hamburguesa") {
+    precio = 80;
+    return precio;
+  }
+  else if (p == "Ensalada") {
+    precio = 60;
+    return precio;
+  }
+    else if (p == "Chilaquiles") {
+    precio = 90;
+    return precio;
+  }
+}
+
+//Funcion para la opcion Huesped
+void huesped() {
+  char roomServ;
+  string hab;
+  string nombreH;
+  string telefonoH;
+  string platilloN;
+  string ingredientesP;
+  float precioPlatillo;
+  float hospedaje;
+  float cuentaH;
+  cout << "Bienvenido, indique a que nombre registrar su estancia: "; cin >> nombreH;
+  cout << "Ingrese un número telefónico: "; cin >> telefonoH;
+  cout << "¿Por cuántas noches se va a quedar?"; cin >> hospedaje;
+  cout << "Indique el tipo de habitación que desea: (Chica, Mediana, Grande) "; cin >> hab;
+  cout << "¿Desea añadir un platillo a su habitación?: (Y/N)"; cin >> roomServ;
+  switch (roomServ) {
+    case 'Y':
+      platilloN = menuP();
+      ingredientesP = menuIngredientes(platilloN);
+      precioPlatillo = menuPrecioPlatillos(platilloN);
+      break;
+    case 'N':
+      platilloN = "Sin platillo";
+      ingredientesP = "Sin platillo";
+      precioPlatillo = 0;
+      break;
+    default:
+      cout << "Opcion inválida" << endl;
+      huesped();
+  }
+  Platillo platillo{platilloN, ingredientesP, precioPlatillo};
+  Habitacion habitacion{numHab(), hab, hospedaje};
+  Huesped huesped{nombreH, telefonoH, cuentaH, habitacion, platillo};
+  huesped.verNombreH();
+  huesped.verTelefonoH();
+  habitacion.verNumeroHab();
+  habitacion.verTamHabitacion();
+  platillo.verNombreP();
+  platillo.verIngredientes();
+  platillo.verPrecioP();
+  huesped.verCuentaH();
+}
+
+//Funcion para la opcion empleado
+void empleado() {
+  int diaCredencial;
+  int mesCredencial;
+  string areaCredencial;
+  string nombreEmpleado;
+  string matriculaEmpleado;
+  float salarioEmpleado;
+  cout << "Ingrese su primer nombre: "; cin >> nombreEmpleado;
+  cout << "Ingrese su matricula: "; cin >> matriculaEmpleado;
+  cout << "Ingrese el dia de vencimiento de su credencial: "; cin >> diaCredencial;
+  cout << "Ingrese el mes de vencimiento de su credencial: "; cin >> mesCredencial;
+  cout << "Ingrese su area: (Gerencia/Limpieza) "; cin >> areaCredencial;
+  if (areaCredencial == "Gerencia") {
+    salarioEmpleado = 10000;
+  }
+  else if (areaCredencial == "Limpieza") {
+    salarioEmpleado = 5000;
+  }
+  else {
+    cout << "Ingrese un area valida" << endl;
+    empleado();
+  }
+  Credencial credencial{diaCredencial, mesCredencial, areaCredencial};
+  Empleado empleado{nombreEmpleado, matriculaEmpleado, salarioEmpleado, credencial};
+  empleado.verNombreE();
+  empleado.verMatriculaE();
+  empleado.verSalarioE();
+  empleado.verCredencial();
+}
+
 int main() {
-  Habitacion habitacion{1, "chica"};
-  Huesped huesped{"Victor", "5500000000", 0, habitacion};
-  Platillo platillo{"Hamburguesa", "Pan, Lechuga, Carne, Queso", 150};
-  Empleado empleado{"Jose", "L0000", 5000};
-  huesped.verHabitacion();
-  huesped.vernombreH();
-  huesped.vertelefonoH();
-  habitacion.vernumeroHab();
-  habitacion.vertamHabitacion();
-  platillo.vernombreP();
-  platillo.verIngrediente();
-  platillo.verprecioP();
-  empleado.vernombreE();
-  empleado.vermatriculaE();
-  empleado.versalarioE();
+  SetConsoleOutputCP(CP_UTF8);
+  char o;
+  cout << "¿Eres empleado o huésped? (E/H) "; cin >> o;
+  switch (o) {
+    case 'H':
+      huesped();
+      main();
+    case 'E':
+      empleado();
+      main();
+    default:
+      cout << "Opcion inválida";
+      main();
+  }
 }
